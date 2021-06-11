eaf4474e1e2dcd7efbd61ec0f850b3bdba38d967
Sofya Inglikova <sofiainglikova@gmail.com>
2021-06-11 11:24:05 +0300

    fxml for auth scene added

---
+++ src/main/java/client/controllers/LoginSceneController.java
@@ -0,0 +1,16 @@
+package client.controllers;
+
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+public class LoginSceneController {
+    public ImageView image;
+
+    public TextField usernameField;
+    public Label usernameLabel;
+    public Label passwordLabel;
+    public PasswordField passwordField;
+    public CheckBox registerCheckBox;
+    public Button signInButton;
+    public Label connectionLabel;
+}
